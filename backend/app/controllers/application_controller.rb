ffb45239d835d32f34cabca415d90ed246f25eb5
Ian Iversen <ianiversen1@gmail.com>
2022-08-23 12:58:29 -0400

    new migrations, seeds, some controllers turned on

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -18,41 +18,41 @@ class ApplicationController < Sinatra::Base
     ticket.to_json(only: [:price])
   end
 
-  post "/shows" do
-    shows = Show.create(
-      show_name: params[:show_name],
-      show_time: params[:show_time],
-      musician: params[:musician],
-      tickets_left: params[:tickets_left]
-    )
-    shows.to_json
-  end
+  # post "/shows" do
+  #   shows = Show.create(
+  #     show_name: params[:show_name],
+  #     show_time: params[:show_time],
+  #     musician: params[:musician],
+  #     tickets_left: params[:tickets_left]
+  #   )
+  #   shows.to_json
+  # end
 
-  patch "/shows/:id" do
-    shows = Show.find(params[:id])
-    shows.update(
-      tickets_left: params[:tickets_left] 
-    )
-    review.to_json
-  end
+  # patch "/shows/:id" do
+  #   shows = Show.find(params[:id])
+  #   shows.update(
+  #     tickets_left: params[:tickets_left] 
+  #   )
+  #   review.to_json
+  # end
 
-  delete "/shows/:id" do 
-    show = Show.find(params[:id])
-    show.destroy
-    show.to_json
-  end
+  # delete "/shows/:id" do 
+  #   show = Show.find(params[:id])
+  #   show.destroy
+  #   show.to_json
+  # end
 
-  post "/users" do
-    users = User.create(
-      full_name: params[:full_name],
-      email: params[:email]
-    )
-    users.to_json
-  end
+  # post "/users" do
+  #   users = User.create(
+  #     full_name: params[:full_name],
+  #     email: params[:email]
+  #   )
+  #   users.to_json
+  # end
 
-  delete "/users/:id" do 
-    users = User.find(params[:id])
-    users.destroy
-    users.to_json
-  end
+  # delete "/users/:id" do 
+  #   users = User.find(params[:id])
+  #   users.destroy
+  #   users.to_json
+  # end
 end
