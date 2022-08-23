1b35ef0f3e94252fbdc71ca97cdb426cf5803ef3
Ian Iversen <ianiversen1@gmail.com>
2022-08-23 08:25:10 -0400

    seed file set up, needs to be completed next

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,5 +1,19 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+
+Show.destroy_all
+Ticket.destroy_all
+User.destroy_all
+Show.reset_pk_sequence
+Ticket.reset_pk_sequence
+User.reset_pk_sequence
+
+Show.create(show_name: "Wilco")
+Show.create(show_name: "Lady Gaga")
+Show.create(show_name: "AC/DC")
+Show.create(show_name: "Lil Nas X")
+Show.create(show_name: "Ariana Grande")
+
+
 
 puts "✅ Done seeding!"
