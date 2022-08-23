fdef27a0b543365f9b37e11ca143adb8ff82ca9f
Ian Iversen <ianiversen1@gmail.com>
2022-08-23 12:31:42 -0400

    simple search bar and header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '60vh',
+      }}>
+        <h1>TicketRuler</h1>
+        <input placeholder="search for an upcoming show..." style={{width: "200px", height: "50px"}}></input>
+      </div>
+      <div>
+        <h2>nice</h2>
+      </div>
+
+
+      
     </div>
-  );
+  )
 }
 
 export default App;
