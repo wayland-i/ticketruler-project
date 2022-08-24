27106b8ae39448ee9ad31a6badc5d5dca55bb85e
Jonathon <jwbialas@gmail.com>
2022-08-24 08:36:36 -0500

    searchbar

--- frontend/src/SearchBar.js
+++ frontend/src/SearchBar.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+
+// const[filter, setFilter] = useState("")
 
 function SearchBar() {
+
+    function search(e){
+        // setFilter((filter)=>{e.target.value})
+        console.log("fdsf")
+    }
+
     return (
         <div className="SearchBar">
-            <input placeholder="Search for a show!" />
+            <input onChange = {search} placeholder="Search for a show!" />
         </div>
     )
 }
 
 export default SearchBar
+
+
+
+
+
